8dafff870d59f1bb32986a8f4424017906493685
KanekiOnMC <talalsatouri@gmail.com>
2025-11-19 04:25:09 +0100

    Revert "Update install.sh"
    This reverts commit b47d933114ccfbb9275d825c623530be24d9036d.

--- install.sh
+++ install.sh
@@ -131,7 +131,7 @@ main() {
 
     # Continue with your existing installation code here
     echo -n "Please enter username for proot installation: " > /dev/tty
-    read username < "default"
+    read username < /dev/tty
 
     # Change repository
 if ! termux-change-repo; then
